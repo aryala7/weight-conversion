047ec5c9a79755d2255354ac9c2c4630f768e8c9
arya <arya.lavasani@gmail.com>
2023-08-03 17:15:02 +0330

    [feat] implemnent test

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -1,6 +1,6 @@
 <?php
 
-it('can convert kilograms to pounds', function () {
+it('can convert kilograms to pounds correctly', function () {
     $pounds = \Aryala7\WeightConversion\Weight::kilograms(100)->toPounds();
     expect($pounds)->toEqual(2.2046);
 });
